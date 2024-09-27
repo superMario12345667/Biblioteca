1bd3063722ae0786333e1bb8dc182a6915c3eca7
RonaldoGM <rg23006@esfe.agape.edu.sv>
2024-09-27 16:28:02 -0600

    Se agregaron los servicios, DTO y la parte de Pages de Autores y Libros

---
+++ DTO/AutorDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Biblioteca.DTO
+{
+    public class AutorResponse
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Nacionalidad { get; set; } = null!;
+
+        public DateOnly FechaNacimiento { get; set; }
+
+        public byte Estado { get; set; }
+    }
+}
---
+++ Services/AutorService.cs
@@ -0,0 +1,41 @@
+﻿using Biblioteca.DTO;
+using System.Net.Http.Headers;
+namespace Biblioteca.Services
+{
+    public class AutorService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthService _authService;
+
+        public AutorService(HttpClient httpClient, AuthService authService)
+        {
+            _httpClient = httpClient;
+            _authService = authService;
+        }
+        public async Task<List<AutorResponse>> GetAutores()
+        {
+
+            try
+            {
+                var token = await _authService.GetToken();
+
+                if (string.IsNullOrEmpty(token))
+                {
+                    throw new InvalidOperationException("El token es nulo o invalido");
+                }
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await _httpClient.GetFromJsonAsync<List<AutorResponse>>("api/autores");
+
+                return response;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception("Error al obtener autores. Revisar conexión a internet.");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ha ocurrido un error inesperado al obtener autores.");
+            }
+        }
+    }
+}
