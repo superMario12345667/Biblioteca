647b20de390c102ad9c45554385f72e705ccc378
EMERSON RIVAS <rd23002@esfe.agape.edu.sv>
2024-09-27 21:19:29 -0600

    Creacion de estado prestamo y prestamos

---
+++ DTO/EstadoPrestamoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Biblioteca.DTO
+{
+    public class EstadoPrestamoResponse
+    {
+        public int IdEstadoPrestamo { get; set; }
+
+        public string NombreEstado { get; set; } = null!;
+    }
+}
---
+++ Services/RolesUsuarioService.cs
@@ -0,0 +1,43 @@
+﻿using Biblioteca.DTO;
+using System.Net.Http.Headers;
+
+namespace Biblioteca.Services
+{
+    public class RolesUsuarioService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthService _authService;
+
+        public RolesUsuarioService(HttpClient httpClient, AuthService authService)
+        {
+            _httpClient = httpClient;
+            _authService = authService;
+        }
+
+        public async Task<List<RolesUsuarioResponse>> GetRolesUsuarios()
+        {
+            try
+            {
+                var token = await _authService.GetToken();
+
+                if (string.IsNullOrEmpty(token))
+                {
+                    throw new InvalidOperationException("El token es nulo o inválido");
+                }
+
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await _httpClient.GetFromJsonAsync<List<RolesUsuarioResponse>>("api/rolesusuarios");
+
+                return response;
+            }
+            catch (HttpRequestException)
+            {
+                throw new Exception("Error al obtener roles de usuario. Revisar conexión a internet.");
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ha ocurrido un error inesperado al obtener roles de usuario.");
+            }
+        }
+    }
+}
